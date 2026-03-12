ae65eef0c5c038478284ee38a28adc9ed67be85f
ayaz990 <ayazmemon07@gmail.com>
2026-03-12 14:03:54 +0530

    Update: add HeroSlider, WhyChooseUs, AboutSection, Certifications to homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,18 @@
+import HeroSlider from "@/components/home/HeroSlider";
 import ProductsShowcase from "@/components/home/ProductsShowcase";
+import WhyChooseUs from "@/components/home/WhyChooseUs";
+import AboutSection from "@/components/home/AboutSection";
+import Certifications from "@/components/home/Certifications";
 import ContactForm from "@/components/home/ContactForm";
 
 export default function Home() {
   return (
     <>
+      <HeroSlider />
       <ProductsShowcase />
+      <WhyChooseUs />
+      <AboutSection />
+      <Certifications />
       <ContactForm />
     </>
   );
